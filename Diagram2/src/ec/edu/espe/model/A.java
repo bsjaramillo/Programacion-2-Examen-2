7f34989a93d69c3640bd3989081f73f3e86ba9de
bsjaramillo <brandonja1995@gmail.com>
2018-01-15 11:42:02 -0500

    Implements the second diagram

---
+++ Diagram2/src/ec/edu/espe/model/A.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author BJ
+ */
+public class A {
+    private B b1;
+    private B b2;
+    public void m1(B x,B y){
+        
+    }
+}
---
+++ Diagram2/src/ec/edu/espe/model/E.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author BJ
+ */
+public class E {
+    private A a;
+    private B[] b;
+    public void m2(C z){
+        
+    }
+    
+}
