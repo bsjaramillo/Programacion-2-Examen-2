bb7999ecc61b22728be69491f86566351f28648c
bsjaramillo <brandonja1995@gmail.com>
2018-01-15 11:41:21 -0500

    Implements the first diagram

---
+++ Diagram1/src/ec/edu/espe/model/D.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author BJ
+ */
+public class D extends A {
+    private E e[];
+    private F f[];
+    
+}
